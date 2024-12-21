163e5247faf6a7f5329c63e71d95ce8c85212a0a
Artem <arcelsium@gmail.com>
2024-12-21 17:08:05 +0300

    prepare jwt

--- project17/CurseController.go
+++ project17/CurseController.go
@@ -1,13 +1,80 @@
 package curseController
 
 import (
+	"errors"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
+
+var (
+	jwtSecret string = "piertogshderjkweigfhnS]ERHBJK-2W9GJHW0EVINWEROW3HN4G0PW3ENFV"
+)
+
+func generateJWT(user *User) (string, error) {
+	claims := &Claims{
+		UserId: user.Id,
+		Role:   user.Email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)), // Срок действия токена - 24 часа
+			IssuedAt:  jwt.NewNumericDate(time.Now()),                     // Время выдачи токена
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(jwtSecret))
+
+	return tokenString, err
+}
+
+func authMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Получаем токен из заголовка Authorization
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "токен отсутствует"})
+			c.Abort()
+			return
+		}
+
+		// Извлекаем токен из заголовка
+		tokenString := authHeader[len("Bearer "):]
+
+		// Парсим токен
+		claims := &Claims{}
+		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+			return []byte(jwtSecret), nil
+		})
+
+		if err != nil {
+			if err == jwt.ErrSignatureInvalid {
+				c.JSON(http.StatusUnauthorized, gin.H{"error": "неверная подпись токена"})
+			} else {
+				c.JSON(http.StatusBadRequest, gin.H{"error": "неверный токен"})
+			}
+			c.Abort()
+			return
+		}
+
+		if !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "недействительный токен"})
+			c.Abort()
+			return
+		}
+
+		// Сохраняем данные пользователя в контексте запроса
+		c.Set("userId", claims.UserId)
+		c.Set("role", claims.Role)
+
+		// Продолжаем выполнение запроса
+		c.Next()
+	}
+
+}
 
 func prepareDB(db *gorm.DB) error {
 
@@ -38,8 +105,30 @@ func dbInit() (*gorm.DB, error) {
 	return db, nil
 }
 
-func usrIsValid(usr *User) bool {
-	if usr.Id != 0 || usr.Age < 1 || usr.Age > 100 || !strings.Contains(usr.Email, "@") || usr.Email == "" || usr.Name == "" || usr.Password == "" || usr.Role == "" {
+type validator interface {
+	IsValid() bool
+}
+
+func validate(v validator) bool {
+	return v.IsValid()
+}
+
+func (user User) IsValid() bool {
+	if user.Id != 0 || user.Age < 1 || user.Age > 100 || !strings.Contains(user.Email, "@") || user.Email == "" || user.Name == "" || user.Password == "" || user.Role == "" {
+		return false
+	}
+	return true
+}
+
+func (course Course) IsValid() bool {
+	if course.endDt.IsZero() || course.startDt.IsZero() || course.Capacity == 0 || course.Name == "" || course.Title == "" {
+		return false
+	}
+	return true
+}
+
+func (logPrms LoginParams) IsValid() bool {
+	if logPrms.Email == "" || !strings.Contains(logPrms.Email, "@") || logPrms.Password == "" {
 		return false
 	}
 	return true
@@ -54,7 +143,7 @@ func addUser(db *gorm.DB) gin.HandlerFunc {
 			c.Request.Body.Close()
 			return
 		}
-		if !usrIsValid(&user) {
+		if !validate(user) {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "получены некорректные данные"})
 			c.Request.Body.Close()
 			return
@@ -69,6 +158,119 @@ func addUser(db *gorm.DB) gin.HandlerFunc {
 	}
 }
 
+func addCurse(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var course Course
+		err := c.ShouldBindJSON(&course)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+			c.Request.Body.Close()
+			return
+		}
+		if validate(course) {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "получены некорректные данные"})
+			c.Request.Body.Close()
+			return
+		}
+		result := db.Create(&course)
+		if result.Error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error})
+			c.Request.Body.Close()
+			return
+		}
+		c.JSON(http.StatusCreated, gin.H{"added course": course})
+	}
+}
+
+func admAllUsers(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		role, _ := c.Get("role")
+		if role.(string) != "admin" {
+			c.JSON(http.StatusLocked, gin.H{"error": "недостаточно прав."})
+			c.Request.Body.Close()
+			return
+		}
+		var users []User
+
+		result := db.Find(&users)
+
+		if result.Error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error})
+			c.Request.Body.Close()
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"users": users})
+	}
+}
+
+func admAllCourses(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		role, _ := c.Get("role")
+		if role.(string) != "admin" {
+			c.JSON(http.StatusLocked, gin.H{"error": "недостаточно прав."})
+			c.Request.Body.Close()
+			return
+		}
+		var courses []Course
+
+		result := db.Find(&courses)
+
+		if result.Error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error})
+			c.Request.Body.Close()
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"courses": courses})
+	}
+}
+
+func login(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var loginParams LoginParams
+		var user User
+
+		// Привязываем JSON к структуре loginParams
+		err := c.ShouldBindJSON(&loginParams)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Валидируем данные
+		if !validate(loginParams) {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "получены некорректные данные"})
+			return
+		}
+
+		// Ищем пользователя в базе данных
+		result := db.Where("email = ? AND password = ?", loginParams.Email, loginParams.Password).First(&user)
+
+		// Проверяем, была ли ошибка
+		if result.Error != nil {
+			// Если пользователь не найден, возвращаем 401
+			if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+				c.JSON(http.StatusUnauthorized, gin.H{"error": "неверный email или пароль"})
+				return
+			}
+
+			// Если другая ошибка, возвращаем 500
+			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+			return
+		}
+
+		// Генерируем JWT-токен
+		token, err := generateJWT(&user)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Возвращаем токен
+		c.JSON(http.StatusOK, gin.H{"token": token})
+	}
+}
 func CurseController() {
 
 	db, err := dbInit()
@@ -77,7 +279,18 @@ func CurseController() {
 	}
 	router := gin.Default()
 
+	router.GET("/login", login(db))
+
+	// Операции администратора.
+	router.GET("/users", admAllUsers(db))
+	router.GET("/courses", admAllCourses(db))
+
+	// Операции с пользователями.
+	// Доступно всем.
 	router.POST("/user", addUser(db))
+	// Операции с курсами.
+	// Только с авторизацией.
+	router.POST("/course", authMiddleware(), addCurse(db))
 
 	router.Run("localhost:8081")
 }
--- project17/structs.go
+++ project17/structs.go
@@ -2,8 +2,11 @@ package curseController
 
 import (
 	"time"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
+// Структура пользователя.
 type User struct {
 	Id       int    `gorm:"primaryKey" json:"-"`
 	Age      int    `gorm:"unique; not null" json:"Age"`
@@ -13,8 +16,9 @@ type User struct {
 	Role     string `gorm:"default:'user'" json:"Role"`
 }
 
+// Структура курса.
 type Course struct {
-	Id       int       `gorm:"primaryKey" json:"Id"`
+	Id       int       `gorm:"primaryKey" json:"-"`
 	Capacity int       `gorm:"not null" json:"Capacity"`
 	Name     string    `gorm:"unique; not null" json:"Name"`
 	Title    string    `gorm:"not null" json:"Title"`
@@ -22,9 +26,23 @@ type Course struct {
 	endDt    time.Time `json:"endDt"`
 }
 
+// Структура записи на курс.
 type Enrollment struct {
 	Id       int       `gorm:"primaryKey" json:"Id"`
 	UserId   uint      `json:"UserId"`
 	CourseId uint      `json:"CourseId"`
 	Date     time.Time `gorm:"default:current_timestamp" json:"Date"`
+}
+
+// Структура авторизированного пользователя.
+type Claims struct {
+	UserId int    `json:"user_id"`
+	Role   string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+// Структура для авторизации пользователя
+type LoginParams struct {
+	Email    string `json:"Email"`
+	Password string `json:"Password"`
 }
